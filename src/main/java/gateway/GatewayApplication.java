c1b26e8df1e3839a8d8f449cef20456f92c4618e
testuser <testuser@example.com>
2020-02-24 16:59:37 -0600

    first

---
+++ src/main/java/gateway/GatewayApplication.java
@@ -0,0 +1,40 @@
+package gateway;
+
+import gateway.filters.ErrorFilter;
+import gateway.filters.PostFilter;
+import gateway.filters.PreFilter;
+import gateway.filters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public PreFilter preFilter(){
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter(){
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter(){
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter(){
+        return new RouteFilter();
+    }
+
+}
---
+++ src/main/java/gateway/filters/PreFilter.java
@@ -0,0 +1,33 @@
+package gateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run(){
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        System.out.println("Request Method: " + " " + request.getMethod() + " " + "Request URL : " + " " + request.getRequestURL().toString());
+        return null;
+    }
+}
